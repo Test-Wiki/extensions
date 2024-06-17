d9ed7759d9753d6734a97f3ac771d7ab83bd76b7
Justarandomamerican <76439161+Justarandomamerican@users.noreply.github.com>
2024-06-17 16:02:54 -0600

    Report extension

---
+++ Report/Report.i18n.alias.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Aliases for Report
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/**
+ * English
+ * @author Ken Hilton
+ */
+$specialPageAliases['en'] = [
+	'Report' => [ 'Report' ],
+	'HandleReports' => [ 'HandleReports', 'Handle Reports' ]
+];
+
+/**
+ * Japanese
+ * @author apple502j
+ */
+$specialPageAliases['ja'] = [
+	'Report' => [ '報告' ],
+	'HandleReports' => [ '報告された版の確認' ]
+];
+
+/**
+ * Turkish
+ * @author ahmetlii
+ */
+$specialPageAliases['tr'] = [
+	'Report' => [ 'Bildirim' ],
+	'HandleReports' => [ 'BildirimleriKontrolEt', 'Bildirimleri Kontrol Et' ]
+];
